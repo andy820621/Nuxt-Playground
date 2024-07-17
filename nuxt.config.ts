75ca36e623fdd0cb9ba7f8b3a1dfa9213e613ca6
BarZ <andy820621@gmail.com>
2024-07-18 04:56:44 +0800

    chore: switch back to nuxt fixture

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,6 @@ export default defineNuxtConfig({
 	nitro: {
 		routeRules: {
 			'/*': {
-				cors: true,
 				headers: {
 					'Cross-Origin-Embedder-Policy': 'require-corp',
 					'Cross-Origin-Opener-Policy': 'same-origin'
