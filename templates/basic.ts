625194a0e3af6eca3dca28b81765cf2caa7fdb5b
BarZ <andy820621@gmail.com>
2024-07-22 23:19:08 +0800

    refactor: rename File to VirtualFile

--- templates/basic.ts
+++ templates/basic.ts
@@ -1,4 +1,4 @@
-import { File } from '../structures/File'
+import { VirtualFile } from '../structures/VirtualFile'
 import { filesToWebContainerFs } from './utils'
 
 export function loadTemplate() {
@@ -13,7 +13,7 @@ export function loadTemplate() {
 
   const files = Object.entries(rawInput)
     .map(([path, content]) => {
-      return new File(path.replace('./basic/', ''), content)
+      return new VirtualFile(path.replace('./basic/', ''), content)
     })
 
   const tree = filesToWebContainerFs(
