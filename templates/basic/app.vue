633557c2a9c56b654caf1e882e46890af264dd35
BarZ <andy820621@gmail.com>
2024-07-23 00:18:51 +0800

    fix(template): move communication logic to plugin

--- templates/basic/app.vue
+++ templates/basic/app.vue
@@ -1,22 +1,3 @@
-<script setup lang="ts">
-const { isHydrating } = useNuxtApp()
-
-// if (process.client) {
-if (isHydrating) {
-  const route = useRoute()
-  watch(
-    () => route.fullPath,
-    (newFullPath) => {
-      window.parent.postMessage({
-        type: 'update:path',
-        path: newFullPath,
-      }, '*')
-    },
-    { immediate: true },
-  )
-}
-</script>
-
 <template>
   <NuxtPage />
 </template>
