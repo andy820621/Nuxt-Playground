8d78db0058045191123b02643263f6bc97ff621b
BarZ <andy820621@gmail.com>
2024-07-27 08:12:26 +0800

    fix: remove content doc max width

--- components/PanelDocs.vue
+++ components/PanelDocs.vue
@@ -7,7 +7,7 @@
       <div i-ph-book-duotone />
       <span text-sm>Guide</span>
     </div>
-    <article class="prose p6 of-auto">
+    <article class="max-w-none prose" of-auto p6>
       <ContentDoc />
     </article>
   </div>
