1f16997d826bb9a97ce3b49617cf04ab15135e32
BarZ <andy820621@gmail.com>
2024-07-28 07:06:49 +0800

    chore: fix bundling

--- templates/basic.ts
+++ templates/basic.ts
@@ -3,6 +3,9 @@ import { filesToWebContainerFs } from './utils'
 import type { TemplateOptions } from './types'
 
 export default function load(options: TemplateOptions = {}) {
+  if (import.meta.server)
+    throw new Error('This template can only be used on the client')
+
   const rawInput = import.meta.glob([
     './basic/**/*.*',
     './basic/**/.layer-playground/**/*.*',
