75e7671739e55c458d6827de10af7f4bf67a3d2c
BarZ <andy820621@gmail.com>
2024-07-28 09:17:43 +0800

    feat: match themes for monaco

--- monaco/worker.ts
+++ monaco/worker.ts
@@ -7,24 +7,23 @@ import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
 import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
+// TODO: material-theme-palenight's format it not compatible with monaco
+// import themeDark from 'shiki/themes/vitesse-dark.mjs'
+// import themeLight from 'shiki/themes/vitesse-light.mjs'
+
 self.MonacoEnvironment = {
   getWorker(_: any, label: string) {
     if (label === 'json')
       return new jsonWorker()
-
     if (label === 'css' || label === 'scss' || label === 'less')
       return new cssWorker()
-
     if (label === 'html' || label === 'handlebars' || label === 'razor')
       return new htmlWorker()
-
     if (label === 'typescript' || label === 'javascript')
       return new tsWorker()
-
     return new editorWorker()
   },
 }
-
 monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
   ...monaco.languages.typescript.typescriptDefaults.getCompilerOptions(),
   noUnusedLocals: false,
@@ -35,3 +34,10 @@ monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
 })
 
 monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true)
+
+// const darkColors = (themeDark as any).colors as Record<string, string>
+// darkColors['editor.background'] = '#00000000'
+// darkColors['editor.lineHighlightBackground'] = '#00000000'
+
+// monaco.editor.defineTheme('theme-light', themeLight as any)
+// monaco.editor.defineTheme('theme-dark', themeDark as any)
