8ce7cdde327deda1794f71fa77c505c720597f96
BarZ <andy820621@gmail.com>
2024-08-15 03:13:09 +0800

    feat: improve command palette

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -8,6 +8,28 @@ const repo = 'https://github.com/andy820621/Nuxt-Playground'
 
 const buildTime = new Date(runtime.public.buildTime)
 const timeAgo = useTimeAgo(buildTime)
+
+addCommands(
+  {
+    id: 'download-zip',
+    title: 'Download playground as ZIP',
+    visible: () => {
+      return play.status === 'ready' && guide.features.download !== false
+    },
+    handler: () => {
+      downloadZip(play.webcontainer!)
+    },
+    icon: 'i-ph-download-duotone',
+  },
+  {
+    id: 'toggle-terminal',
+    title: 'Toggle terminal',
+    handler: () => {
+      ui.toggleTerminal()
+    },
+    icon: 'i-ph-terminal-window-duotone',
+  },
+)
 </script>
 
 <template>
